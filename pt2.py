0dcbbe243d65d61a20402612e6c0981fc123567f
Honnest <daichi.hiki@gmail.com>
2026-09-20 21:56:41 +0300

    Remove test and temp files from tracking

--- pt2.py
+++
@@ -1,6 +0,0 @@
-﻿p = "test_apply2.py"
-s = open(p, encoding="utf-8").read()
-s = s.replace("for key, label, _i, _l in mod.fields.CORE_FIELDS: app.core_vars[key] = FakeVar()",
-              "for entry in mod.fields.CORE_FIELDS: app.core_vars[entry[0]] = FakeVar()")
-open(p, "w", encoding="utf-8").write(s)
-print("patched")
